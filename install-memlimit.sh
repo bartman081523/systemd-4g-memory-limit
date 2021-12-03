f41088b6cea5468a8cfbef948b2e4572b9e07507
chlorophyll-zz <43696399+chlorophyll-zz@users.noreply.github.com>
2021-12-03 12:54:46 +0100

    Create install-memlimit.sh

---
+++ install-memlimit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo mkdir -p /etc/systemd/system/user-.slice.d/
+sudo rm /etc/systemd/system/user-.slice.d/50-memory.conf
+sudo echo "[Slice]" >> /etc/systemd/system/user-.slice.d/50-memory.conf
+sudo echo "MemoryMax=3300M" >> /etc/systemd/system/user-.slice.d/50-memory.conf
+sudo echo "DefaultMemoryAccounting=yes" >> /etc/systemd/system.conf
+sudo systemctl daemon-reload
+echo "Done. Please reboot!"
